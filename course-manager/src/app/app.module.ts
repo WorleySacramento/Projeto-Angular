8679d0f3495bf5baeaebabbef5f9e67e216b32e2
BrotherTech-BH <78088577+WorleySacramento@users.noreply.github.com>
2021-09-09 22:35:33 -0300

    up

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { CourseListComponent } from './courses/courseList.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponent,
   ],
   imports: [
     BrowserModule,
---
+++ course-manager/src/app/courses/courseList.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+  selector: 'app-course-list',
+  templateUrl:"./courseList.component.html"
+})
+
+export class CourseListComponent {
+
+
+
+}
